a66b300921d0b2bd6d8781d244dc50dd4aa9c107
Guy Amzaleg <guyamzaleg@gmail.com>
2024-08-12 18:01:04 +0300

    Update SpellSimple.java

---
+++ SpellSimple.java
@@ -0,0 +1,18 @@
+public class SpellSimple {
+    private String name;
+    private String words;
+
+    SpellSimple(String name, String words) {
+        this.name=name;
+        this.words=words;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public String getWords(){
+        return this.words;
+    }
+
+}
